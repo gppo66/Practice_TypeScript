a9b281d55ebfceef9bdff49ff26ceb1b68b95443
gppo66 <gppo66@gmail.com>
2022-09-25 15:57:54 +0900

    string_number_array_type

---
+++ typescript_02/index.ts
@@ -0,0 +1,13 @@
+// JS String
+var str = "hello";
+
+// TS string
+const str2: string = "hello";
+
+// TS number
+const num: number = 10;
+
+// TS array
+const arr: Array<number> = [1, 2, 3];
+const arr2: Array<string> = ["captin"];
+const arr3: number[] = [1, 2, 3];
